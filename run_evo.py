6c074062049bd97e7e9970db64f34fcdff444818
ASFaust <andreas.s.faust@gmail.com>
2025-12-25 11:33:14 +0100

    fix

--- run_evo.py
+++ run_evo.py
@@ -4,13 +4,14 @@ from evo.task import SolarAntsTask
 
 evo = Evo(
     task=SolarAntsTask(n_trials=3),
-    population_size=10,
-    elite_k=2,
+    population_size=1000,
+    elite_k=100,
     model_lr=1e-3,
     dataset_max=100_000,
     seed=42,
 )
 
 evo.initialize_population()
-
-evo.step_generation()
+for i in range(500):
+    print(f"Generation {i}")
+    evo.step_generation()
